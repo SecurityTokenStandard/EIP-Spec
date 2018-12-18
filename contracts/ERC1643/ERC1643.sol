acb7a4ec89411b8b6cfed6bec94bf51f8283849d
satyam <satyam@secureblocks.io>
2018-12-18 19:42:26 +0530

    finalize the contract implementation

--- contracts/ERC1643/ERC1643.sol
+++ contracts/ERC1643/ERC1643.sol
@@ -3,19 +3,80 @@ pragma solidity ^0.4.24;
 import "./IERC1643.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
+/**
+ * @title Standard implementation of ERC1643 Document management
+ */
 contract ERC1643 is IERC1643, Ownable {
 
     struct Document {
         bytes32 docHash; // Hash of the document
+        uint256 lastModified; // Timestamp at which document details was last modified
         string uri; // URI of the document that exist off-chain
     }
 
+    // mapping to store the documents details in the document
     mapping(bytes32 => Document) internal _documents;
-
+    // mapping to store the document name indexes
+    mapping(bytes32 => uint256) internal _docIndexes;
+    // Array use to store all the document name present in the contracts
     bytes32[] _docNames;
     
-    function setDocument(bytes32 _name, string _uri, bytes32 _documentHash) external onlyOwner
+    /**
+     * @notice Used to attach a new document to the contract, or update the URI or hash of an existing attached document
+     * @dev Can only be executed by the owner of the contract.
+     * @param _name Name of the document. It should be unique always
+     * @param _uri Off-chain uri of the document from where it is accessible to investors/advisors to read.
+     * @param _documentHash hash (of the contents) of the document.
+     */
+    function setDocument(bytes32 _name, string _uri, bytes32 _documentHash) external onlyOwner {
+        require(_name != bytes32(0), "Zero value is not allowed");
+        require(keccak256(_uri) != keccak256(""), "Should not be a empty uri");
+        if (_documents[_name].lastModified == uint256(0)) {
+            _docNames.push(_name);
+            _docIndexes[_name] = _docNames.length;
+        }
+        _documents[_name] = Document(_documentHash, now, _uri);
+        emit DocumentUpdated(_name, _uri, _documentHash, now);
+    }
 
+    /**
+     * @notice Used to remove an existing document from the contract by giving the name of the document.
+     * @dev Can only be executed by the owner of the contract.
+     * @param _name Name of the document. It should be unique always
+     */
+    function removeDocument(bytes32 _name) external onlyOwner {
+        require(_documents[_name].lastModified != uint256(0), "Document should be existed");
+        uint256 index = _docIndexes[_name] - 1;
+        if (index != _docNames.length - 1) {
+            _docNames[index] = _docNames[_docNames.length - 1];
+            _docIndexes[_name] = index + 1; 
+        }
+        _docNames.length--;
+        emit DocumentRemoved(_name, _documents[_name].uri, _documents[_name].docHash, now);
+        delete _documents[_name];
+    }
 
+    /**
+     * @notice Used to return the details of a document with a known name (`bytes32`).
+     * @param _name Name of the document
+     * @return string The URI associated with the document.
+     * @return bytes32 The hash (of the contents) of the document.
+     * @return uint256 the timestamp at which the document was last modified.
+     */
+    function getDocument(bytes32 _name) external view returns (string, bytes32, uint256) {
+        return (
+            _documents[_name].uri,
+            _documents[_name].docHash,
+            _documents[_name].lastModified
+        );
+    }
+
+    /**
+     * @notice Used to retrieve a full list of documents attached to the smart contract.
+     * @return bytes32 List of all documents names present in the contract.
+     */
+    function getAllDocuments() external view returns (bytes32[]) {
+        return _docNames;
+    }
 
 }
