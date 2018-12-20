626bb9507a859342c87632ca362b4470924ff316
satyam <satyam@secureblocks.io>
2018-12-20 15:08:08 +0530

    intial draft of implementation of EIP 1594

---
+++ contracts/ERC1594/ERC1594.sol
@@ -0,0 +1,220 @@
+pragma solidity ^0.4.24;
+
+import "./IERC1594.sol";
+import "./ERC20Token.sol";
+import "../math/KindMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * @title Standard implementation of ERC1594 (Subset of ERC1400 https://github.com/ethereum/EIPs/issues/1411)
+ */
+contract ERC1594 is IERC1594, ERC20Token, Ownable {
+
+    // Variable which tells whether issuance is ON or OFF forever
+    // Implementers need to implement one more function to reset the value of `issuance` variable
+    // to false. That function is not a part of the standard (EIP-1594) as it is depend on the various factors
+    // issuer, followed compliance rules etc. So issuers have the choice how they want to close the issuance. 
+    bool private issuance = true;
+    
+    /// Constructor
+    constructor() public  {
+
+    }
+    
+    /**
+     * @notice Transfer restrictions can take many forms and typically involve on-chain rules or whitelists.
+     * However for many types of approved transfers, maintaining an on-chain list of approved transfers can be
+     * cumbersome and expensive. An alternative is the co-signing approach, where in addition to the token holder
+     * approving a token transfer, and authorised entity provides signed data which further validates the transfer.
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     * @param _data The `bytes _data` allows arbitrary data to be submitted alongside the transfer.
+     * for the token contract to interpret or record. This could be signed data authorising the transfer
+     * (e.g. a dynamic whitelist) but is flexible enough to accomadate other use-cases.
+     */
+    function transferWithData(address _to, uint256 _value, bytes _data) external {
+        _transferWithDataRestricted(msg.sender, _to, _value, _data);
+    }
+
+    /**
+     * @notice Transfer restrictions can take many forms and typically involve on-chain rules or whitelists.
+     * However for many types of approved transfers, maintaining an on-chain list of approved transfers can be
+     * cumbersome and expensive. An alternative is the co-signing approach, where in addition to the token holder
+     * approving a token transfer, and authorised entity provides signed data which further validates the transfer.
+     * @dev `msg.sender` MUST have a sufficient `allowance` set and this `allowance` must be debited by the `_value`.
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     * @param _data The `bytes _data` allows arbitrary data to be submitted alongside the transfer.
+     * for the token contract to interpret or record. This could be signed data authorising the transfer
+     * (e.g. a dynamic whitelist) but is flexible enough to accomadate other use-cases.
+     */
+    function transferFromWithData(address _from, address _to, uint256 _value, bytes _data) external {
+        _transferFromWithDataRestricted(_from, _to, _value, _data);
+    }
+
+    /**
+     * @notice Internal function of `transferWithData()` which consist all the checks related to balance and zero value.
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     * @param _data The `bytes _data` allows arbitrary data to be submitted alongside the transfer.
+     */
+    function _transferWithDataRestricted(address _from, address _to, uint256 _value, bytes _data) internal {
+        // Add a function to validate the `_data` parameter with restriction
+        _transferRestricted(_from, _to, _value);
+    }
+
+    /**
+     * @notice Internal function of `transferFromWithData()` which consist all the checks related to balance and zero value.
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     * @param _data The `bytes _data` allows arbitrary data to be submitted alongside the transfer.
+     */
+    function _transferFromWithDataRestricted(address _from, address _to, uint256 _value, bytes _data) internal {
+        // Add function to validate the `_data` parameter with restrictions
+        transferFrom(_from, _to, _value);
+    }
+
+    /**
+     * @notice Internal function of `transferWithData()` which doesn't consist all the sanity checks
+     * related to balance and zero value.
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     * @param _data The `bytes _data` allows arbitrary data to be submitted alongside the transfer.
+     */
+    function _transferWithData(address _from, address _to, uint256 _value, bytes _data) internal {
+        // Add a function to validate the `_data` parameter
+        _transfer(_from, _to, _value);
+    }
+
+    /**
+     * @notice Internal function of `transferFromWithData()` which doesn't consist all the sanity checks
+     * related to balance and zero value.
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     * @param _data The `bytes _data` allows arbitrary data to be submitted alongside the transfer.
+     */
+    function _transferFromWithData(address _from, address _to, uint256 _value, bytes _data) internal {
+        // Add a function to validate the `_data` parameter
+        _transferFrom(msg.sender, _from, _to, _value);
+    }
+
+    /**
+     * @notice A security token issuer can specify that issuance has finished for the token
+     * (i.e. no new tokens can be minted or issued).
+     * @dev If a token returns FALSE for `isIssuable()` then it MUST always return FALSE in the future.
+     * If a token returns FALSE for `isIssuable()` then it MUST never allow additional tokens to be issued.
+     * @return bool `true` signifies the minting is allowed. While `false` denotes the end of minting
+     */
+    function isIssuable() external view returns (bool) {
+        return issuance;
+    }
+
+    /**
+     * @notice This function must be called to increase the total supply (Corresponds to mint function of ERC20).
+     * @dev It only be called by the token issuer or the operator defined by the issuer. ERC1594 doesn't have
+     * have the any logic related to operator but its superset ERC1400 have the operator logic and this function
+     * is allowed to call by the operator.
+     * @param _tokenHolder The account that will receive the created tokens (account should be whitelisted or KYCed).
+     * @param _value The amount of tokens need to be issued
+     * @param _data The `bytes _data` allows arbitrary data to be submitted alongside the transfer.
+     */
+    function issue(address _tokenHolder, uint256 _value, bytes _data) external onlyOwner {
+        // Add a function to validate the `_data` parameter
+        require(issuance, "Issuance is closed");
+        _mint(_tokenHolder, _value);
+        emit Issued(msg.sender, _tokenHolder, _value, _data);
+    }
+
+    /**
+     * @notice This function redeem an amount of the token of a msg.sender. For doing so msg.sender may incentivize
+     * using different ways that could be implemented with in the `redeem` function definition. But those implementations
+     * are out of the scope of the ERC1594. 
+     * @param _value The amount of tokens need to be redeemed
+     * @param _data The `bytes _data` it can be used in the token contract to authenticate the redemption.
+     */
+    function redeem(uint256 _value, bytes _data) external {
+        // Add a function to validate the `_data` parameter
+        _burn(msg.sender, _value);
+        emit Redeemed(address(0), msg.sender, _value, _data);
+    }
+
+    /**
+     * @notice This function redeem an amount of the token of a msg.sender. For doing so msg.sender may incentivize
+     * using different ways that could be implemented with in the `redeem` function definition. But those implementations
+     * are out of the scope of the ERC1594. 
+     * @dev It only be called by the token issuer or the operator defined by the issuer. ERC1594 doesn't have
+     * have the any logic related to operator but its superset ERC1400 have the operator logic and this function
+     * is allowed to call by the operator.
+     * @param _tokenHolder The account whose tokens gets redeemed.
+     * @param _value The amount of tokens need to be redeemed
+     * @param _data The `bytes _data` it can be used in the token contract to authenticate the redemption.
+     */
+    function redeemFrom(address _tokenHolder, uint256 _value, bytes _data) external onlyOwner {
+        // Add a function to validate the `_data` parameter
+        _burnFrom(_tokenHolder, _value);
+        emit Redeemed(msg.sender, msg.sender, _value, _data);
+    }
+
+    /**
+     * @notice Transfers of securities may fail for a number of reasons. So this function will used to understand the
+     * cause of failure by getting the byte value. Which will be the ESC that follows the EIP 1066. ESC can be mapped 
+     * with a reson string to understand the failure cause, table of Ethereum status code will always reside off-chain
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     * @param _data The `bytes _data` allows arbitrary data to be submitted alongside the transfer.
+     * @return bool It signifies whether the transaction will be executed or not.
+     * @return byte Ethereum status code (ESC)
+     * @return bytes32 Application specific reason code 
+     */
+    function canTransfer(address _to, uint256 _value, bytes _data) external view returns (bool, byte, bytes32) {
+        if (_balances[msg.sender] < _value)
+            return (false, 0x52, bytes32(0));
+
+        else if (_to == address(0))
+            return (false, 0x57, bytes32(0));
+
+        else if (!KindMath.sub(_balances[msg.sender], _value) || !KindMath.add(_balances[_to], _value))
+            return (false, 0x50, bytes32(0));
+
+        _transferWithData(msg.sender, _to, _value, _data);
+        return (true, 0x51, bytes32(0));
+    }
+
+    /**
+     * @notice Transfers of securities may fail for a number of reasons. So this function will used to understand the
+     * cause of failure by getting the byte value. Which will be the ESC that follows the EIP 1066. ESC can be mapped 
+     * with a reson string to understand the failure cause, table of Ethereum status code will always reside off-chain
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     * @param _data The `bytes _data` allows arbitrary data to be submitted alongside the transfer.
+     * @return bool It signifies whether the transaction will be executed or not.
+     * @return byte Ethereum status code (ESC)
+     * @return bytes32 Application specific reason code 
+     */
+    function canTransferFrom(address _from, address _to, uint256 _value, bytes _data) external view returns (bool, byte, bytes32) {
+        if (_value > _allowed[_from][msg.sender])
+            return (false, 0x53, bytes32(0));
+
+        else if (!KindMath.sub(_allowed[_from][msg.sender], _value))
+            return (false, 0x50, bytes32(0));
+
+        else if (_balances[_from] < _value)
+            return (false, 0x52, bytes32(0));
+
+        else if (_to == address(0))
+            return (false, 0x57, bytes32(0));
+
+        else if (!KindMath.sub(_balances[_from], _value) || !KindMath.add(_balances[_to], _value))
+            return (false, 0x50, bytes32(0));
+
+        _transferFromWithData(_from, _to, _value, _data);
+        return (true, 0x51, bytes32(0));
+    }
+
+}
---
+++ test/erc1594.js
@@ -0,0 +1,7 @@
+contract('erc1594', function(accounts) {
+  it("should assert true", function(done) {
+    var erc1594 = erc1594.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
